b653b85f8305f12b6284f9eb590f793441c1682b
hkidd <harrisonakidd@gmail.com>
2021-08-23 20:19:32 -0500

    removed some unused code

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,5 +1,4 @@
 import React from "react";
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const styles = {
   header: {
@@ -32,40 +31,6 @@ export default function Contact() {
         <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
       </div>
       </form>
-
-      {/* <h2 class="follow-me">Follow Me</h2>
-      <ul>
-        <li>
-          <a
-            href="https://www.linkedin.com/in/harrison-kidd/"
-            target="_blank"
-            rel="noreferrer"
-            class="fa fa-linkedin icon"
-          ><FontAwesomeIcon icon={['fab', 'linkedin']} />
-            <span style={styles.span}>My LinkedIn</span>
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://twitter.com/HarrisonKidd8"
-            target="_blank"
-            rel="noreferrer"
-            class="fa fa-twitter icon"
-          ><FontAwesomeIcon icon={['fab', 'twitter']} />
-            <span style={styles.span}>My Twitter</span>
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://www.instagram.com/harrisonkidd/"
-            target="_blank"
-            rel="noreferrer"
-            class="fa fa-instagram icon"
-          ><FontAwesomeIcon icon={['fab', 'instagram']} />
-            <span style={styles.span}>My Instagram</span>
-          </a>
-        </li>
-      </ul> */}
     </div>
   );
 }
